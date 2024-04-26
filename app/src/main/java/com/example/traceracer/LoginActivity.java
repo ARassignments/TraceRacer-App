142b7ecf7a92f859186ea0c5927353814241dccb
AR Assignments <abdurrehman905623@gmail.com>
2024-04-26 16:32:02 +0500

    Login

--- app/src/main/java/com/example/traceracer/LoginActivity.java
+++ app/src/main/java/com/example/traceracer/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.traceracer;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,14 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.layout_nine);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
+
     }
 }
