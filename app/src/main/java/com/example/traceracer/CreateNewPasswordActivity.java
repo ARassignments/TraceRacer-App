8a33064c5a2d2316296895cd07ff3ba29d481149
AR Assignments <abdurrehman905623@gmail.com>
2024-04-27 16:23:08 +0500

    Update Screens

--- app/src/main/java/com/example/traceracer/CreateNewPasswordActivity.java
+++ app/src/main/java/com/example/traceracer/CreateNewPasswordActivity.java
@@ -1,10 +1,9 @@
 package com.example.traceracer;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class CreateNewPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        TextView forgotBtn = findViewById(R.id.forgotBtn);
-        forgotBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_create_new_password);
+
+        Button resetBtn = findViewById(R.id.resetBtn);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,ResetPasswordActivity.class);
+                Intent intent = new Intent(CreateNewPasswordActivity.this, PasswordChangedActivity.class);
                 startActivity(intent);
             }
         });
