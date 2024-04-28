4eba79323a2d98cc74745af7e6bc14bbe1a5fac0
AptatekSoftwares <168176239+AptatekSoftwares@users.noreply.github.com>
2024-04-28 12:03:40 +0500

    b

--- app/src/main/java/com/example/traceracer/LoginActivity.java
+++ app/src/main/java/com/example/traceracer/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.traceracer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
-    }
-}
+        TextView forgotpwd = findViewById(R.id.forgotpwd);
+        forgotpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, Reset_Passwrord_Activity.class);
+                startActivity(intent);
+            }
+        }
