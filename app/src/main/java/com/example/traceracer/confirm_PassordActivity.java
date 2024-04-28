d036cd585dd29dd7b69a29cdeddeb2d01e27c1f4
AptatekSoftwares <168176239+AptatekSoftwares@users.noreply.github.com>
2024-04-28 11:54:04 +0500

    n

---
+++ app/src/main/java/com/example/traceracer/confirm_PassordActivity.java
@@ -0,0 +1,20 @@
+package com.example.traceracer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class confirm_PassordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_confirm_passord);
+
+    }
+}
