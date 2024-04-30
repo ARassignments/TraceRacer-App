07aac61d020b0fef9a3e9d578a12e4906dde7a79
AptatekSoftwares <168176239+AptatekSoftwares@users.noreply.github.com>
2024-04-30 13:34:59 +0500

    d

--- app/src/main/java/com/example/traceracer/PasswordChangedActivity.java
+++ app/src/main/java/com/example/traceracer/PasswordChangedActivity.java
@@ -1,6 +1,9 @@
 package com.example.traceracer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,15 @@ public class PasswordChangedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_password_changed);
+        Button lgnBtn = findViewById(R.id.lgnBtn);
+        lgnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PasswordChangedActivity.this, LoginActivity.class);
+                startActivity(intent);
 
+
+            }
+        });
     }
 }
