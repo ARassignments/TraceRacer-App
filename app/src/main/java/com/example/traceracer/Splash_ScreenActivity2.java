ceed99a80d5b31a56eb7100b1c99346ad254bf3a
AptatekSoftwares <168176239+AptatekSoftwares@users.noreply.github.com>
2024-05-01 09:25:49 +0500

    j

--- app/src/main/java/com/example/traceracer/Splash_ScreenActivity2.java
+++ app/src/main/java/com/example/traceracer/Splash_ScreenActivity2.java
@@ -1,6 +1,5 @@
 package com.example.traceracer;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash_ScreenActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen2);
+
     }
 }
