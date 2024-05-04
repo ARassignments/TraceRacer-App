7d93876604bfd9d4ee4de56ccbad426af26b9e8a
shazsoomro <116937161+shazsoomro@users.noreply.github.com>
2024-05-04 01:14:26 -0700

    UPDATE

---
+++ app/src/main/java/com/example/traceracer/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.traceracer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Sing_up_Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        TextView lgnbt = findViewById(R.id.aa);
+        lgnbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Sing_up_Activity2.super.onBackPressed();
+            }
+        });
+
+    }
+}
